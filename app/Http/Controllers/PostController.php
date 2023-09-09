b121cd102122d71a4e4b2c449835638ee54768f1
syuzrh27 <syuja27@gmail.com>
2023-09-09 14:04:25 +0700

    init

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function productionhouse(){
+        return view('about');
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+</head>
+<body class="bg-gray-100">
+
+<div class="min-h-screen flex items-center justify-center">
+    <div class="bg-white w-3/4 p-10 rounded-lg shadow-lg text-center">
+        <h2 class="text-3xl font-semibold mb-6">About Our Production House</h2>
+        <p class="text-gray-700 leading-loose">We are a creative production house dedicated to bringing your ideas to life. With a team of passionate professionals, we create stunning content for various media platforms, including film, television, and digital media.</p>
+        <p class="text-gray-700 leading-loose mt-4">Our mission is to deliver high-quality, compelling, and visually captivating stories that engage and entertain audiences worldwide. We pride ourselves on our commitment to excellence and creativity in every project we undertake.</p>
+    </div>
+</div>
+</body>
+</html>
